c36c5009f9cb39c5c97f7465edab73d9566f138d
speechkey <gremoz@gmail.com>
2012-05-11 22:36:42 +0200

    Calling a less binary through command executer

--- less.py
+++ less.py
@@ -1,23 +1,28 @@
 import sublime
 import sublime_plugin
 import subprocess
+import commandline
 
 
 class CompileLessOnSave(sublime_plugin.EventListener):
 
     def on_post_save(self, view):
+        try:
+            less = commandline.find_binary('less')
+        except commandline.BinaryNotFoundError:
+            sublime.error_message("I couldn't find \"less\" binary on your system. Less is required on your system. Please install it and try again.")
+            return
+
         if not view.file_name().endswith('.less'):
             return
 
         filename = view.file_name()
         output_filename = filename.replace('.less', '.css')
 
-        proc = subprocess.Popen("lessc %s" % filename,
-            shell=True,
-            stdout=subprocess.PIPE)
-        output = proc.communicate()[0]
+        command = [less] + [filename]
+        output = commandline.execute(command)
 
-	output_file = open(output_filename,"w")
-	output_file.write(output)
-	output_file.close()
+        output_file = open(output_filename,"w")
+        output_file.write(output)
+        output_file.close()
 
